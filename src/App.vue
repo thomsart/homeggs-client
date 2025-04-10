27a746af9c122a3fabb08ca49f15c32664407273
thomas cottenceau <thomascottenceau@M4-thomas.local>
2025-04-10 17:37:51 +0200

    creation of homelanding page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script>
 
-    
+    import { ref } from 'vue'
+
+    export default {
+        setup() {
+            // Déclaration d'une référence booléenne
+            const token = ref(true);
+
+            return {
+                token
+            };
+        }
+    }
+
 </script>
 
 <template>
-    <h1>Welcome to Homeggs!</h1>
+    
+    <h1>Hommeggs</h1>
+    <template v-if="token===true">
+        <h2>Mets tout tes œufs dans le même foyer!</h2>
+    </template>
+
+    <template v-else>
+        <h2>Bienvenue!</h2>
+    </template>
+
 </template>
 
 <style>
