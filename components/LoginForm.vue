cae8e6a5120b7e114577b24f6ada449d48c73a1b
thomas cottenceau <thomascottenceau@M4-thomas.local>
2025-04-14 16:29:05 +0200

    LoginForm.vue

---
+++ components/LoginForm.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+    import CustomButton from './CustomButton.vue';
+
+    import { ref, watchEffect } from 'vue'
+    
+</script>
+
+<template>
+
+    <form @submit.prevent="">
+        <input v-model="email" type="email" placeholder="your email">
+        <input v-model="password" type="password" placeholder="your password">
+        <CustomButton buttonText="connexion" buttonCollor="blue"></CustomButton>
+    </form>
+
+</template>
+
+<style>
+
+
+
+</style>
