510693ce7014df266b3650c5a32f97cfd5d2c873
thomas cottenceau <thomascottenceau@M4-thomas.local>
2025-04-15 09:50:23 +0200

    import of pinia in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 // main.js
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 const app = createApp(App)
-
+app.use(pinia)
 app.mount('#app');
