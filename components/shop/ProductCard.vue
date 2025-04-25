6a78c71bbcdd21e5c133a3790ba7e6b817e31476
thomas cottenceau <thomascottenceau@M4-thomas.local>
2025-04-25 12:57:33 +0200

    add of folder shop and file ProductCard.vue

---
+++ components/shop/ProductCard.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+    import { reactive } from 'vue'
+    import { callShop } from '../../utils/api/callShopEndpoints.js'
+    import Product from '../../models/shop/product.js'
+
+    import CustomButton from '../CustomButton.vue'
+
+    
+
+</script>
+
+<template>
+
+
+
+</template>
+
+<style>
+
+
+
+</style>
